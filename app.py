e332e931619c99d56f6e3ead868964266c4f679b
szerencsi.kristof <szerencsi.kristof@gmail.com>
2024-08-03 21:12:03 +0200

    add new stuff

--- app.py
+++ app.py
@@ -5,22 +5,22 @@ app = Flask(__name__)
 # Sample product data
 products = {
     'hot_or_not': {
-        'name': 'Hot or Not?! - O.b.G. Edition',
+        'name': 'Hot or Not?!',
         'description': 'This is a special one! Our podium cocktail at the Austrian Bartender Awards. Old brand. Old logo. Old bottle. The same popular taste and characteristics of: Vodka, Vermout and our Sangria-Blend with Cherry, Strawberry, Spices and much more... much more love! ...pssst, stock is selling out fast and will never be produced again in this form, so hurry and get yourself any of our Old but Gold cocktails.',
         'image': 'images/cocktail2.jpg'
     },
     'what_the_melon': {
-        'name': 'What the Melon?! - O.b.G. Edition',
+        'name': 'What the Melon?! ',
         'description': 'Old brand. Old logo. Old bottle. The same popular taste and characteristics of: Vodka, Watermelon, Lime, Mint.',
         'image': 'images/cocktail3.jpg'
     },
     'horny_bae': {
-        'name': 'Horny Bae - O.b.G. Edition',
+        'name': 'Horny Bae',
         'description': 'Old brand. Old logo. Old bottle. The same popular taste and characteristics of: White Rum, Cucumber, Pineapple, Almond, Melon.',
         'image': 'images/cocktail4.jpg'
     },
     'wet_ginger': {
-        'name': 'Wet Ginger - O.b.G. Edition',
+        'name': 'Wet Ginger',
         'description': 'Old brand. Old logo. Old bottle. The same popular taste and characteristics of: Gin, Ginger, Lemon, Cucumber, Pink Pepper.',
         'image': 'images/cocktail5.jpg'
     }
